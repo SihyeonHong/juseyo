33433bbd66bf5cb4559f54b57424fd5962a05769
SihyeonHong <hongsihyeon179@gmail.com>
2026-05-18 12:36:00 +0900

    chore: 린트 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,61 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+import unusedImportsPlugin from "eslint-plugin-unused-imports";
+import prettierConfig from "eslint-config-prettier";
+
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      "unused-imports": unusedImportsPlugin,
+    },
+    rules: {
+      // import 관련 규칙
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "import/no-duplicates": "error",
+      "import/no-unresolved": "off",
+
+      // React Hooks 규칙
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+
+      // 사용하지 않는 import 제거
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+
+      // 기본 규칙들
+      "no-unused-vars": "off",
+      "prefer-const": "warn",
+    },
+  },
+  prettierConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
